34a7ea4d3bace8ea5b2d59e188d693ea02a5851e
ContentsViewer <fivetwothreesix@gmail.com>
2017-11-02 07:00:32 +0900

    パターンデータ作成

--- Source/Pattern/Pattern.h
+++ Source/Pattern/Pattern.h
@@ -4,21 +4,148 @@
 
 static const uint8_t pattern0[] PROGEMEM =
 {
-
+    0x7e,
+    0x81,
+    0x81,
+    0x81,
+    0x81,
+    0x7e
 
 };
 
 
+static const uint8_t pattern2[] PROGEMEM =
 {
-
+    0x82,
+    0xc1,
+    0xe1,
+    0x91,
+    0x89,
+    0x86
 };
 
 
+static const uint8_t pattern3[] PROGEMEM =
 {
-
+    0x41,
+    0x81,
+    0x81,
+    0x8f,
+    0x93,
+    0x61
 };
 
 
+static const uint8_t pattern4[] PROGEMEM =
+{
+    0xe0,
+    0xd0,
+    0x8c,
+    0x82,
+    0x81,
+    0x80
+};
+
+
+static const uint8_t pattern5[] PROGEMEM =
+{
+    0x47,
+    0x84,
+    0x84,
+    0x84,
+    0x80,
+    0x78
+};
+
+
+static const uint8_t pattern6[] PROGEMEM =
+{
+    0x7c,
+    0x9a,
+    0x99,
+    0x99,
+    0x91,
+    0x60
+};
+
+
+static const uint8_t pattern7[] PROGEMEM =
+{
+    0x01,
+    0xe1,
+    0x11,
+    0x09,
+    0x05,
+    0x03
+};
+
+
+
+static const uint8_t pattern8[] PROGEMEM =
+{
+    0x66,
+    0x99,
+    0x99,
+    0x99,
+    0x99,
+    0x66
+};
+
+
+static const uint8_t pattern9[] PROGEMEM =
+{
+    0x06,
+    0x89,
+    0x99,
+    0x59,
+    0x59,
+    0x3e
+};
+
+
+static const uint8_t patternVerticalLine[] PROGEMEM =
+{
+    0xff
+};
+
+
+static const uint8_t patternHorizontalLine[] PROGEMEM =
+{
+    0x01,
+    0x01,
+    0x01,
+    0x01,
+    0x01,
+    0x01,
+    0x01,
+    0x01
+};
+
+
+static const uint8_t patternSlash[] PROGEMEM =
+{
+    0x80,
+    0x40,
+    0x20,
+    0x10,
+    0x08,
+    0x04,
+    0x02,
+    0x01
+};
+
+
+static const uint8_t patternBackSlash[] PROGEMEM =
+{
+    0x01,
+    0x02,
+    0x04,
+    0x08,
+    0x10,
+    0x20,
+    0x40,
+    0x80
+};
 
 
 
