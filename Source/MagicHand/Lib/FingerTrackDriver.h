5fa7c168eceef8a617edbf7fc2c8bbc981435fd0
miyokichi <33227799+miyokichi@users.noreply.github.com>
2017-11-05 14:43:13 +0900

    figertrackdrive.h置き換え

--- Source/MagicHand/Lib/FingerTrackDriver.h
+++ Source/MagicHand/Lib/FingerTrackDriver.h
@@ -16,108 +16,105 @@
 class FingerTrackDriver : public TrackBallDriver
 {
 private:
-    const float sensitivity = 4.0;
-
-    const int X_AXIS_DIR = -1;
-    const int Y_AXIS_DIR = -1;
-
+    const float sensitivity = 32.0;
 
     // 最大周期
     // 人がトラックボールをゆっくり回せる限界周期
-    const int maxRange = 30000;
+    const int maxRange = 10000;
 
     // 最小周期
     // 人がトラックボールを速く回せる限界周期
-    const int minRange = 900;
+    const int minRange = 500;
 
-    double deltaLeft = 0.0;
-    double deltaRight = 0.0;
-    double deltaUp = 0.0;
-    double deltaDown = 0.0;
+    long leftSum = 0;
+    long rightSum = 0;
+    long upSum = 0;
+    long downSum = 0;
 
 public:
     const float range = 1.0 / minRange - 1.0 / maxRange;
 
-    double deltaX = 0.0;
-    double deltaY = 0.0;
+    int deltaX = 0;
+    int deltaY = 0;
 
 
 
 public:
 
     //セマフォ用に四方向を個別にdeltaX,deltaYに追加
-    void AddDeltaLeftToDeltaX(void) {
-
-        deltaX -= X_AXIS_DIR * deltaLeft;
-
-        deltaLeft = 0.0;
+    void AddLeftToDeltaX(void) {
+        if (leftSum > 0) {
+            deltaX -= leftSum;
+            leftSum = 0;
+        }
     }
 
-    void AddDeltaRightToDeltaX(void) {
+    void AddRightToDeltaX(void) {
+        if (rightSum > 0) {
 
-        deltaX += X_AXIS_DIR * deltaRight;
-
-        deltaRight = 0.0;
+            deltaX += rightSum;
+            rightSum = 0;
+        }
     }
 
-    void AddDeltaUpToDeltaY(void) {
-        deltaY += Y_AXIS_DIR * deltaUp;
-
-        deltaUp = 0.0;
+    void AddUpToDeltaY(void) {
+        if (upSum > 0) {
+            deltaY -= upSum;
+            upSum = 0;
+        }
     }
 
-    void AddDeltaDownToDeltaY(void) {
-        deltaY -= Y_AXIS_DIR * deltaDown;
+    void AddDownToDeltaY(void) {
+        if (downSum > 0) {
 
-        deltaDown = 0.0;
+            deltaY += downSum;
+            downSum = 0;
+        }
     }
 
-    double GetDeltaX(void) {
+    int GetDeltaX(void) {
         return deltaX;
     }
 
-    double GetDeltaY(void) {
+    int GetDeltaY(void) {
         return deltaY;
     }
 
     void ResetDeltaXY(void) {
-        deltaX = 0.0;
-        deltaY = 0, 0;
+        deltaX = 0;
+        deltaY = 0;
     }
 
     void ResetDeltaX(void) {
-        deltaX = 0, 0;
+        deltaX = 0;
     }
 
     void ResetDeltaY(void) {
-        deltaY = 0, 0;
+        deltaY = 0;
     }
 
 
 
-    void AddLeftToDelta(void) {
-
-        //Serial.println(left);
-
-        if (left > 0) {
-            deltaLeft += sensitivity / left / range;
+    void AddLeftToSum(void) {
+        if(left){
+            leftSum++;
         }
     }
-    void AddRightToDelta(void) {
-        if (right > 0) {
-            deltaRight += sensitivity / right / range;
+    void AddRightToSum(void) {
+        if(right){
+            rightSum++;
         }
     }
 
-    void AddUpToDelta(void) {
-        if (up > 0) {
-            deltaUp += sensitivity / up / range;
+    void AddUpToSum(void) {
+        if(up){
+            upSum++;
         }
     }
 
-    void AddDownToDelta(void) {
-        if (down > 0) {
-            deltaDown += sensitivity / down / range;
+    void AddDownToSum(void) {
+        if(down){
+            downSum++;
         }
     }
 
@@ -137,7 +134,7 @@ public:
     //  他タスクが長時間そのリソースにアクセスできません.
 
 
-
+    /*更新につき、TrackBallDriverクラスと同じなので使わない。
     // --- 上下左右値取得関数 -------------------------------------------------
 
     //
@@ -149,34 +146,21 @@ public:
     // left値をトラックボールデバイスから読み込みます.
     //
     void ReadLeft(void) {
-
         left = pulseIn(pinLFT, HIGH, timeout);
-        //Serial.println(left);
-        if (left >= maxRange || left < minRange) {
-            //Serial.println("+");
-            left = 0;
-        }
-        //Serial.println("+");
     }
 
     //
     // right値をトラックボールデバイスから読み込みます.
     //
     void ReadRight(void) {
-        right = pulseIn(pinRHT, HIGH, timeout);
-        if (right >= maxRange || right < minRange) {
-            right = 0;
-        }
+        right = pulseIn(pinRHT, HIGH, timeout);}
     }
 
     //
     // up値をトラックボールデバイスから読み込みます.
     //
     void ReadUp(void) {
-        up = pulseIn(pinUP, HIGH, timeout);
-        if (up >= maxRange || up < minRange) {
-            up = 0;
-        }
+        up = pulseIn(pinUP, HIGH, timeout);}
     }
 
 
@@ -185,12 +169,10 @@ public:
     //
     void ReadDown(void) {
         down = pulseIn(pinDWN, HIGH, timeout);
-        if (down >= maxRange || down < minRange) {
-            down = 0;
-        }
     }
 
     // End 上下左右取得関数 -----------------
+    */
 
 };
 
