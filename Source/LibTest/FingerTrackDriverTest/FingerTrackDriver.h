741ef7a4da696a134ea65a6b8b8b43cc71821b31
miyokichi <33227799+miyokichi@users.noreply.github.com>
2017-11-05 14:38:19 +0900

    fingertrackdriver更新

--- Source/LibTest/FingerTrackDriverTest/FingerTrackDriver.h
+++ Source/LibTest/FingerTrackDriverTest/FingerTrackDriver.h
@@ -1,6 +1,11 @@
-//更新履歴
+// 更新履歴
 //  2017/11/2:
-//    完成
+//   完成
+//
+//  2017/11/3:
+//   0わりの問題を修正
+//   一部計算の順番変更(桁落ち対策)
+//
 
 #ifndef FINGER_TRACK_DRIVER_H
 #define FINGER_TRACK_DRIVER_H
@@ -10,111 +15,165 @@
 
 class FingerTrackDriver : public TrackBallDriver
 {
-  private:
-    const byte sensitivity = 32;
-    const int maxRange = 20000;
-    const int minRange = 10;
+private:
+    const float sensitivity = 32.0;
 
-    long leftSum;
-    long rightSum;
-    long upSum;
-    long downSum;
+    // 最大周期
+    // 人がトラックボールをゆっくり回せる限界周期
+    const int maxRange = 10000;
 
-  public:
+    // 最小周期
+    // 人がトラックボールを速く回せる限界周期
+    const int minRange = 500;
+
+    long leftSum = 0;
+    long rightSum = 0;
+    long upSum = 0;
+    long downSum = 0;
+
+public:
     const float range = 1.0 / minRange - 1.0 / maxRange;
 
-    int deltaX;
-    int deltaY;
+    int deltaX = 0;
+    int deltaY = 0;
 
 
 
-  public:
+public:
 
     //セマフォ用に四方向を個別にdeltaX,deltaYに追加
     void AddLeftToDeltaX(void) {
-      deltaX += (int)-1 / leftSum / range * sensitivity;
-      leftSum = 0;
+        if (leftSum > 0) {
+            deltaX -= leftSum;
+            leftSum = 0;
+        }
     }
 
     void AddRightToDeltaX(void) {
-      deltaX += (int)1 / rightSum / range * sensitivity;
-      rightSum = 0;
+        if (rightSum > 0) {
+
+            deltaX += rightSum;
+            rightSum = 0;
+        }
     }
 
     void AddUpToDeltaY(void) {
-      deltaY += (int)-1 / upSum / range * sensitivity;
-      upSum = 0;
+        if (upSum > 0) {
+            deltaY -= upSum;
+            upSum = 0;
+        }
     }
 
     void AddDownToDeltaY(void) {
-      deltaY += (int)1 / downSum / range * sensitivity;
-      downSum = 0;
+        if (downSum > 0) {
+
+            deltaY += downSum;
+            downSum = 0;
+        }
     }
 
     int GetDeltaX(void) {
-      return deltaX;
+        return deltaX;
     }
 
     int GetDeltaY(void) {
-      return deltaY;
+        return deltaY;
     }
 
     void ResetDeltaXY(void) {
-      deltaX = 0;
-      deltaY = 0;
+        deltaX = 0;
+        deltaY = 0;
     }
 
     void ResetDeltaX(void) {
-      deltaX = 0;
+        deltaX = 0;
     }
 
     void ResetDeltaY(void) {
-      deltaY = 0;
+        deltaY = 0;
     }
 
-    void ReadLeft(void) {
-      left = pulseIn(pinLFT, HIGH, timeout);
-      if (left >= maxRange || left < minRange) {
-        left = 0;
-      }
-    }
+
 
     void AddLeftToSum(void) {
-      leftSum += left;
+        if(left){
+            leftSum++;
+        }
     }
-
-    void ReadRight(void) {
-      right = pulseIn(pinRHT, HIGH, timeout);
-      if (right >= maxRange || right < minRange) {
-        right = 0;
-      }
-    }
-
     void AddRightToSum(void) {
-      rightSum += right;
-    }
-
-    void ReadUp(void) {
-      up = pulseIn(pinUP, HIGH, timeout);
-      if (up >= maxRange || up < minRange) {
-        up = 0;
-      }
+        if(right){
+            rightSum++;
+        }
     }
 
     void AddUpToSum(void) {
-      upSum += up;
-    }
-
-    void ReadDown(void) {
-      down = pulseIn(pinDWN, HIGH, timeout);
-      if (down >= maxRange || down < minRange) {
-        down = 0;
-      }
+        if(up){
+            upSum++;
+        }
     }
 
     void AddDownToSum(void) {
-      downSum += down;
+        if(down){
+            downSum++;
+        }
     }
+
+    //
+    // 変数 left, right, up, downについて:
+    //  これらの変数は, それぞれ一つのタスクがアクセスする変数である.
+    //  よって, これら変数に対するアクセス制御は行わない.
+    // 
+    // pulseIn関数について:
+    //  この関数は, パルス幅を返す関数である.
+    //  パルスが入力されるのを指定された時間待機し,
+    //  その間でパルスが入力されたならば, そのパルスの長さを返します.
+    //  その間で入力されなかった場合は, 0を返します.
+    //  
+    //  パルスが入力されなかったとき, 待機することに注意してください.
+    //  この間に, あるリソースに対するアクセス権を持ち続けている際,
+    //  他タスクが長時間そのリソースにアクセスできません.
+
+
+    /*更新につき、TrackBallDriverクラスと同じなので使わない。
+    // --- 上下左右値取得関数 -------------------------------------------------
+
+    //
+    // 警告:
+    //  この関数実行前であらゆるリソースのアクセス権を開放して下さい.
+    //
+
+    //
+    // left値をトラックボールデバイスから読み込みます.
+    //
+    void ReadLeft(void) {
+        left = pulseIn(pinLFT, HIGH, timeout);
+    }
+
+    //
+    // right値をトラックボールデバイスから読み込みます.
+    //
+    void ReadRight(void) {
+        right = pulseIn(pinRHT, HIGH, timeout);}
+    }
+
+    //
+    // up値をトラックボールデバイスから読み込みます.
+    //
+    void ReadUp(void) {
+        up = pulseIn(pinUP, HIGH, timeout);}
+    }
+
+
+    //
+    // down値をトラックボールデバイスから読み込みます.
+    //
+    void ReadDown(void) {
+        down = pulseIn(pinDWN, HIGH, timeout);
+    }
+
+    // End 上下左右取得関数 -----------------
+    */
+
 };
 
 #endif FINGER_TRACK_DRIVER_H
---
+++ Source/LibTest/regacy/FingerTrackDriverTest/FingerTrackDriver.h
@@ -0,0 +1,120 @@
+//更新履歴
+//  2017/11/2:
+//    完成
+
+#ifndef FINGER_TRACK_DRIVER_H
+#define FINGER_TRACK_DRIVER_H
+
+#include "TrackBallDriver.h"
+
+
+class FingerTrackDriver : public TrackBallDriver
+{
+  private:
+    const byte sensitivity = 32;
+    const int maxRange = 20000;
+    const int minRange = 10;
+
+    long leftSum;
+    long rightSum;
+    long upSum;
+    long downSum;
+
+  public:
+    const float range = 1.0 / minRange - 1.0 / maxRange;
+
+    int deltaX;
+    int deltaY;
+
+
+
+  public:
+
+    //セマフォ用に四方向を個別にdeltaX,deltaYに追加
+    void AddLeftToDeltaX(void) {
+      deltaX += (int)-1 / leftSum / range * sensitivity;
+      leftSum = 0;
+    }
+
+    void AddRightToDeltaX(void) {
+      deltaX += (int)1 / rightSum / range * sensitivity;
+      rightSum = 0;
+    }
+
+    void AddUpToDeltaY(void) {
+      deltaY += (int)-1 / upSum / range * sensitivity;
+      upSum = 0;
+    }
+
+    void AddDownToDeltaY(void) {
+      deltaY += (int)1 / downSum / range * sensitivity;
+      downSum = 0;
+    }
+
+    int GetDeltaX(void) {
+      return deltaX;
+    }
+
+    int GetDeltaY(void) {
+      return deltaY;
+    }
+
+    void ResetDeltaXY(void) {
+      deltaX = 0;
+      deltaY = 0;
+    }
+
+    void ResetDeltaX(void) {
+      deltaX = 0;
+    }
+
+    void ResetDeltaY(void) {
+      deltaY = 0;
+    }
+
+    void ReadLeft(void) {
+      left = pulseIn(pinLFT, HIGH, timeout);
+      if (left >= maxRange || left < minRange) {
+        left = 0;
+      }
+    }
+
+    void AddLeftToSum(void) {
+      leftSum += left;
+    }
+
+    void ReadRight(void) {
+      right = pulseIn(pinRHT, HIGH, timeout);
+      if (right >= maxRange || right < minRange) {
+        right = 0;
+      }
+    }
+
+    void AddRightToSum(void) {
+      rightSum += right;
+    }
+
+    void ReadUp(void) {
+      up = pulseIn(pinUP, HIGH, timeout);
+      if (up >= maxRange || up < minRange) {
+        up = 0;
+      }
+    }
+
+    void AddUpToSum(void) {
+      upSum += up;
+    }
+
+    void ReadDown(void) {
+      down = pulseIn(pinDWN, HIGH, timeout);
+      if (down >= maxRange || down < minRange) {
+        down = 0;
+      }
+    }
+
+    void AddDownToSum(void) {
+      downSum += down;
+    }
+};
+
+#endif FINGER_TRACK_DRIVER_H
